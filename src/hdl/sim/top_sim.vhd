4fa2f46b18b2db8a43b986b9c05a86e0591521ea
Tomislav Harmina <tomislav.harmina@gmail.com>
2024-06-18 18:51:11 +0200

    Initial project structure

---
+++ src/hdl/sim/top_sim.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity top_sim is
+
+end top_sim;
+
+architecture Behavioral of top_sim is
+
+begin
+
+
+end Behavioral;
---
+++ src/hdl/top.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity top is
+
+end top;
+
+architecture rtl of top is
+
+begin
+
+
+end rtl;
