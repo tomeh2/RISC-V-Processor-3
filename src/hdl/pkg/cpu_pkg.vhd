2f6baf6c6fbad2a77972e4df7410c391cc4c5ea0
Tomislav Harmina <tomislav.harmina@gmail.com>
2024-06-19 17:49:35 +0200

    Added RV32I opcode definitions to cpu_pkg

--- src/hdl/pkg/cpu_pkg.vhd
+++ src/hdl/pkg/cpu_pkg.vhd
@@ -2,5 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 package cpu_pkg is
-    
+    constant OPCODE_LUI     :  std_logic_vector(6 downto 0) := "0110111";
+    constant OPCODE_AUIPC   :  std_logic_vector(6 downto 0) := "0010111";
+    constant OPCODE_JAL     :  std_logic_vector(6 downto 0) := "1101111";
+    constant OPCODE_JALR    :  std_logic_vector(6 downto 0) := "1100111";
+    constant OPCODE_BRANCH  :  std_logic_vector(6 downto 0) := "1100011";
+    constant OPCODE_LOAD    :  std_logic_vector(6 downto 0) := "0000011";
+    constant OPCODE_STORE   :  std_logic_vector(6 downto 0) := "0100011";
+    constant OPCODE_IMM     :  std_logic_vector(6 downto 0) := "0010011";
+    constant OPCODE_ARITH   :  std_logic_vector(6 downto 0) := "0110011";
+    constant OPCODE_FENCE   :  std_logic_vector(6 downto 0) := "0001111";
+    constant OPCODE_ENV     :  std_logic_vector(6 downto 0) := "1110011";
 end package;
