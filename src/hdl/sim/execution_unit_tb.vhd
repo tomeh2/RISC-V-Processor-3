f4b3c0c3f4b9b463961aa735ed756cd013049d06
Tomislav Harmina <tomislav.harmina@gmail.com>
2024-06-22 17:27:26 +0200

    Added automated correctness checking to the testbench

--- src/hdl/sim/execution_unit_tb.vhd
+++ src/hdl/sim/execution_unit_tb.vhd
@@ -10,13 +10,22 @@ entity execution_unit_tb is
 end execution_unit_tb;
 
 architecture Behavioral of execution_unit_tb is
+    constant MAX_CYCLES : integer := 100000;
+
     signal clk : std_logic;
     signal rst : std_logic;
 
     signal uop : T_uop;
     signal cdb : T_uop;
 
-    type T_uop_memory is array (0 to 31) of T_uop;
+    type T_scoreboard_entry is record
+        uop : T_uop;
+        done : std_logic;
+        time_since_update: unsigned(7 downto 0);
+        valid : std_logic;
+    end record;
+    type T_scoreboard is array (0 to 31) of T_scoreboard_entry;
+    signal scoreboard : T_scoreboard;
 begin
     process
     begin
@@ -28,16 +37,70 @@ begin
 
     rst <= '1', '0' after RST_DURATION;
 
-    process
-        variable uop_memory : T_uop_memory;
+    process(clk)
         variable curr_id : integer := 0;
+        variable cycles : integer := 0;
+        variable uOPs_gen : integer := 0;
+        variable uOPs_pass : integer := 0;
+        variable uOPs_fail : integer := 0;
     begin
-        --wait until rst = '0';
-        for i in range 0 to 31 loop
-            uop <= F_gen_uop_arith(curr_id);
-            uop_memory(curr_id) := uop;
-            curr_id := curr_id + 1;
-        end loop;
+        if rising_edge(clk) then
+            if rst = '0' then
+                cycles := cycles + 1;
+                if cycles = MAX_CYCLES then
+                    report "MAX_CYCLES reached";
+                    report "uOPs Generated = " & integer'image(uOPs_gen);
+                    report "uOPs Passed = " & integer'image(uOPs_pass);
+                    report "uOPs Failed = " & integer'image(uOPs_fail);
+                    assert false severity failure;
+                end if;
+
+                for i in 0 to 31 loop
+                    if scoreboard(i).valid = '1' and scoreboard(i).done = '0' then
+                        scoreboard(i).time_since_update <= scoreboard(i).time_since_update + 1;
+
+                        if scoreboard(i).time_since_update = to_unsigned(255, 8) then
+                            assert false report "Timeout in uOP with ID = " & integer'image(to_integer(unsigned(cdb.id))) severity failure;
+                        end if;
+                    end if;
+                end loop;
+
+                if scoreboard(curr_id).done = '1' or scoreboard(curr_id).valid = '0' or scoreboard(curr_id).valid = 'U' then
+                    uop <= F_gen_uop_arith(curr_id);
+                    scoreboard(curr_id).uop <= uop;
+                    scoreboard(curr_id).done <= '0';
+                    scoreboard(curr_id).valid <= '1';
+                    scoreboard(curr_id).time_since_update <= (others => '0');
+                    curr_id := curr_id + 1;
+                    uOPs_gen := uOPs_gen + 1;
+
+                    if curr_id = 32 then
+                        curr_id := 0;
+                    end if;
+                else
+                    uop <= T_UOP_ZERO;
+                end if;
+            end if;
+        end if;
+
+        if rising_edge(clk) then
+            if cdb.valid = '1' then
+                scoreboard(to_integer(unsigned(cdb.id))).done <= '1';
+                scoreboard(to_integer(unsigned(cdb.id))).valid <= '0';
+                if F_check_uop_arith(cdb) = false then
+                    report "Bad Result:";
+                    report "ID: " & integer'image(to_integer(unsigned(cdb.id)));
+                    report "OP_SEL: " & integer'image(to_integer(unsigned(cdb.op_sel)));
+                    report "OP1: " & integer'image(to_integer(unsigned(cdb.reg_read_1_data)));
+                    report "OP2: " & integer'image(to_integer(unsigned(cdb.reg_read_2_data)));
+                    report "RESULT: " & integer'image(to_integer(unsigned(cdb.reg_write_data)));
+                    uOPs_fail := uOPs_fail + 1;
+                else
+                    uOPs_pass := uOPs_pass + 1;
+                    --report "Good result ID = " & integer'image(to_integer(unsigned(cdb.id)));
+                end if;
+            end if;
+        end if;
     end process;
 
     uut : entity work.execution_unit
