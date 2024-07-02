14f378f25ad749d83aa87942f9828b6c696bfd62
Tomislav Harmina <tomislav.harmina@gmail.com>
2024-07-02 20:01:50 +0200

    Back-end updated to reflect new interfaces

--- src/hdl/back_end/back_end.vhd
+++ src/hdl/back_end/back_end.vhd
@@ -18,6 +18,11 @@ architecture rtl of back_end is
     signal R_pipeline_regfile : T_uop;
     signal R_cdb_eu0 : T_uop;
 
+    signal eu0_stall_in : std_logic;
+    signal eu0_stall_out : std_logic;
+    signal rf_stall_in : std_logic;
+    signal rf_stall_out : std_logic;
+
     signal cdb : T_uop;
 begin
     scheduler_inst : entity work.scheduler
@@ -28,17 +33,23 @@ begin
              clk      => clk,
              reset    => reset);
 
+    rf_stall_in <= eu0_stall_out;
     regfile_inst : entity work.register_file
     port map(uop_1_in   => R_pipeline_sched,
              uop_1_out  => R_pipeline_regfile,
-             cdb_1_in   => cdb,
+             cdb   => cdb,
+             stall_in   => rf_stall_in,
+             stall_out  => rf_stall_out,
              clk        => clk,
              reset      => reset);
 
     eu0_inst : entity work.execution_unit
-    port map(uop    => R_pipeline_regfile,
-             cdb    => R_cdb_eu0,
-             clk    => clk,
-             reset  => reset);
+    port map(uop        => R_pipeline_regfile,
+             cdb        => R_cdb_eu0,
+             stall_in   => eu0_stall_in,
+             stall_out  => eu0_stall_out,
+             clk        => clk,
+             reset      => reset);
     cdb <= R_cdb_eu0;
+    eu0_stall_in <= '0';
 end rtl;
